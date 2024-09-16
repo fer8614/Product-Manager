4b249eba26c47e6e20e32aff8270726470eec4b5
Fernando <fernando.inversionmf@gmail.com>
2024-09-15 21:13:27 -0700

    connect to DB PostgreSQL

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,17 @@
 import express from 'express';
 import router from './router';
 import db from './config/db';
+import colors from 'colors';
 
 //Connect to DB
 async function connectDB() {
     try {
         await db.authenticate();
         db.sync();
-        console.log('Connection has been established successfully.');    
+        console.log( colors.blue.bold ('Connection has been established successfully.'));    
     } catch (error) {
-        console.log(error);
-        console.log('Error connect to DB');
+        // console.log(error);
+        console.log( colors.red.bold ('Error connect to DB'));
     }
 }
 
