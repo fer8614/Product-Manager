378603e50ff3123f110680d444944b1bb1de1113
Fernando <fernando.inversionmf@gmail.com>
2024-09-15 17:23:23 -0700

    dist ignore, gitignore

--- src/config/db.ts
+++ src/config/db.ts
@@ -2,6 +2,6 @@ import { Sequelize } from "sequelize";
 import dotenv from 'dotenv';
 dotenv.config();
 
-const db = new Sequelize(process.env.DATABASE_URL);
+const db = new Sequelize(process.env.DATABASE_URL!);
 
 export default db;
