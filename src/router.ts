6bf8b2771ddbf2e02c65d133b0659f2a8eb03b69
Fernando <fernando.inversionmf@gmail.com>
2024-09-17 20:57:38 -0700

    Creating endpoint (GET) to obtain the products

--- src/router.ts
+++ src/router.ts
@@ -1,15 +1,13 @@
 import { Router } from 'express';
 import { body } from 'express-validator';
-import { createProduct } from './handlers/product';
+import { createProduct, getProducts } from './handlers/product';
 import { handleInputErrors } from './middleware';
 
 const router = Router();
 
 
 //Routing
-router.get('/', (req, res) => {
-    res.json('From GET');
-})
+router.get('/', getProducts); 
 
 router.post( '/', 
     // Validations
