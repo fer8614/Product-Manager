18cfd3df01b32e7806c4f4922fde1ca0235d35ec
Fernando <fernando.inversionmf@gmail.com>
2024-09-17 18:43:27 -0700

    Moving validation to the Router

--- src/router.ts
+++ src/router.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { body } from 'express-validator';
 import { createProduct } from './handlers/product';
 
 const router = Router();
@@ -9,7 +10,21 @@ router.get('/', (req, res) => {
     res.json('From GET');
 })
 
-router.post( '/', createProduct );
+router.post( '/', 
+    // Validations
+    body('name')
+        .notEmpty().withMessage('Product name is required'),
+    body('price')
+        .isNumeric().withMessage('Price must be a number')
+        .withMessage('Price is required')
+        .custom( value => value > 0 ).withMessage('Price must be greater than 0'),
+    // await check('availability').not().isEmpty().withMessage('Availability is required').run(req);
+    // const errors = validationResult(req);
+    // if( !errors.isEmpty() ){
+    //     return res.status(400).json({ errors: errors.array() });
+    // }
+    createProduct 
+);
 
 router.put('/', (req, res) => {
     res.json('From PUT');
