7e64f928b68716752d6beef9b9088f1dc8df414d
Fernando <fernando.inversionmf@gmail.com>
2024-09-18 21:17:50 -0700

    Create endpoint (PATCH)

--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -53,9 +53,26 @@ export const updateProduct = async ( req: Request, res: Response ) => {
 
     // Update product
     await product.update( req.body );
-
     // Get updated product
     product.save();
     
+    res.json({ data: product });
+}
+
+export const updateAvailability = async ( req: Request, res: Response ) => {
+
+    const { id } = req.params;
+    const product = await Product.findByPk( id );
+ 
+    if( !product ) {
+        return res.status(404).json({ 
+            message: 'Product not found' 
+        });
+    }
+
+    // Update product
+    product.availability = !product.dataValues.availability;
+    // Get updated product
+    await product.save();
     res.json({ data: product });
 }
