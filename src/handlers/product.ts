915650cc1ab7849e26558e5ab0bd09fe58bf8e23
Fernando <fernando.inversionmf@gmail.com>
2024-09-21 14:59:45 -0700

    Code optimization after code coverage

--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -3,20 +3,15 @@ import Product from "../models/Product.model";
 import { error } from "console";
 
 export const getProducts = async ( req : Request, res : Response ) => {
-    try {
         const products = await Product.findAll( {
             order: [
                 ['price', 'DESC']
             ]
         });
         res.json({ data: products });
-    } catch (error) {
-        console.log( error );
-    }
 }
 
 export const getProductById = async ( req : Request, res : Response ) => {
-    try {
        const { id } = req.params;
        const product = await Product.findByPk( id );
 
@@ -25,20 +20,12 @@ export const getProductById = async ( req : Request, res : Response ) => {
                 error: 'Product not found' 
             });
        }
-
        res.json({ data: product });
-    } catch (error) {
-        console.log( error );
-    }
 }
 
 export const createProduct = async ( req: Request, res: Response ) => {
-   try {
        const product = await Product.create( req.body );
        res.status(201).json({ data: product });
-   } catch ( error ) {
-       console.log( error );
-   }
 }
 
 export const updateProduct = async ( req: Request, res: Response ) => {
