7e3a81d781af1fd5a69d3b94dd9db6490386a0f4
Fernando <fernando.inversionmf@gmail.com>
2024-09-22 18:54:35 -0700

    Documenting PUT update product

--- src/router.ts
+++ src/router.ts
@@ -1,7 +1,14 @@
-import { Router } from 'express';
-import { body, param } from 'express-validator';
-import { createProduct, deleteProduct, getProductById, getProducts, updateAvailability, updateProduct } from './handlers/product';
-import { handleInputErrors } from './middleware';
+import { Router } from "express";
+import { body, param } from "express-validator";
+import {
+  createProduct,
+  deleteProduct,
+  getProductById,
+  getProducts,
+  updateAvailability,
+  updateProduct,
+} from "./handlers/product";
+import { handleInputErrors } from "./middleware";
 
 const router = Router();
 /**
@@ -51,16 +58,16 @@ const router = Router();
  *     description: Get a list of products
  *     responses:
  *       200:
- *         description: Successful response 
+ *         description: Successful response
  *         content:
  *           application/json:
- *             schema:  
+ *             schema:
  *               type: array
  *               items:
  *                  $ref: '#/components/schemas/Product'
  */
 //Routing
-router.get('/', getProducts); 
+router.get("/", getProducts);
 
 /**
  * @swagger
@@ -71,12 +78,12 @@ router.get('/', getProducts);
  *       - Products
  *     description: Return a product based on unique ID
  *     parameters:
-    *     - in : path
-    *       name: id
-    *       description: The ID if the product to retrieve
-    *       required: true
-    *       schema:
-    *         type: integer
+ *     - in : path
+ *       name: id
+ *       description: The ID if the product to retrieve
+ *       required: true
+ *       schema:
+ *         type: integer
  *     responses:
  *       200:
  *         description: Successful response
@@ -86,15 +93,16 @@ router.get('/', getProducts);
  *               $ref: '#/components/schemas/Product'
  *       404:
  *         description: Product not found
- *         
+ *
  *       400:
  *         description: Bad Request - Invalid ID
  */
-router.get('/:id',
-    param('id').isInt().withMessage('Id must be an integer'), 
-    handleInputErrors,
-    getProductById
-); 
+router.get(
+  "/:id",
+  param("id").isInt().withMessage("Id must be an integer"),
+  handleInputErrors,
+  getProductById,
+);
 
 /**
  * @swagger
@@ -111,7 +119,7 @@ router.get('/:id',
  *           schema:
  *             type: object
  *             properties:
- *               name: 
+ *               name:
  *                 type: string
  *                 example: "Keyboard"
  *               price:
@@ -119,54 +127,107 @@ router.get('/:id',
  *                 example: 101
  *     responses:
  *       201:
- *         description: Product created
- *       
- *       400:       
+ *         description: Successful response
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Product'
+ *
+ *       400:
  *         description: Bad Request - Invalid input
- * 
- * 
- */                     
-router.post( '/', 
-    // Validations
-    body('name')
-        .notEmpty().withMessage('Product name is required'),
-    body('price')
-        .isNumeric().withMessage('Price must be a number')
-        .notEmpty().withMessage('Price is required')
-        .custom( value => value > 0 ).withMessage('Price must be greater than 0'),
-    
-    handleInputErrors,
-    createProduct 
+ *
+ *
+ */
+router.post(
+  "/",
+  // Validations
+  body("name").notEmpty().withMessage("Product name is required"),
+  body("price")
+    .isNumeric()
+    .withMessage("Price must be a number")
+    .notEmpty()
+    .withMessage("Price is required")
+    .custom((value) => value > 0)
+    .withMessage("Price must be greater than 0"),
+
+  handleInputErrors,
+  createProduct,
 );
 
-router.put('/:id', 
-    // Validations
-    param('id')
-        .isInt().withMessage('Id must be an integer'), 
-    body('name')
-        .notEmpty().withMessage('Product name is required'),
-    body('price')
-        .isNumeric().withMessage('Price must be a number')
-        .notEmpty().withMessage('Price is required')
-        .custom( value => value > 0 ).withMessage('Price must be greater than 0'),
-    body('availability')
-        .isBoolean().withMessage('Availability must be a boolean'),
-    handleInputErrors,
-    updateProduct 
+/**
+ * @swagger
+ * /api/products/{id}:
+ *   put:
+ *     summary: Update a product with user input
+ *     tags:
+ *       - Products
+ *     description: Update a product with user input
+ *     parameters:
+ *     - in : path
+ *       name: id
+ *       description: The ID if the product to retrieve
+ *       required: true
+ *       schema:
+ *         type: integer
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *                 example: "Keyboard"
+ *               price:
+ *                 type: number
+ *                 example: 101
+ *               availability:
+ *                 type: boolean
+ *                 example: true
+ *     responses:
+ *       200:
+ *         description: Successful response
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Product'
+ *       400:
+ *         description: Bad Request - Invalid ID or invalid input data
+ *       404:
+ *         description: Product not found
+ */
+router.put(
+  "/:id",
+  // Validations
+  param("id").isInt().withMessage("Id must be an integer"),
+  body("name").notEmpty().withMessage("Product name is required"),
+  body("price")
+    .isNumeric()
+    .withMessage("Price must be a number")
+    .notEmpty()
+    .withMessage("Price is required")
+    .custom((value) => value > 0)
+    .withMessage("Price must be greater than 0"),
+  body("availability")
+    .isBoolean()
+    .withMessage("Availability must be a boolean"),
+  handleInputErrors,
+  updateProduct,
 );
 
-router.patch('/:id', 
-    param('id')
-        .isInt().withMessage('Id must be an integer'), 
-    handleInputErrors,
-    updateAvailability 
+router.patch(
+  "/:id",
+  param("id").isInt().withMessage("Id must be an integer"),
+  handleInputErrors,
+  updateAvailability,
 );
 
-router.delete('/:id',
-    param('id')
-        .isInt().withMessage('Id must be an integer'), 
-    handleInputErrors,
-    deleteProduct
+router.delete(
+  "/:id",
+  param("id").isInt().withMessage("Id must be an integer"),
+  handleInputErrors,
+  deleteProduct,
 );
 
 export default router;
