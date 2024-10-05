c4d27da4fb8a9375fafe9cd625fb2a8a69bdc965
Fernando <fernando.inversionmf@gmail.com>
2024-10-04 20:41:18 -0700

    Configure to Build

--- dist/server.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.sumar = sumar;
-function sumar() {
-    console.log(3 + 3);
-}
-//# sourceMappingURL=server.js.map
